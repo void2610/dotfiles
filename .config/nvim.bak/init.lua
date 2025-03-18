92f3109ff779a952030c88bccb25112c9e4387c8
void2610 <izumisan2610@gmail.com>
2025-03-18 10:28:12 +0900

    install lazyvim

---
+++ .config/nvim.bak/init.lua
@@ -0,0 +1,5 @@
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+
+vim.wo.number = true
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,10 @@
-vim.scriptencoding = 'utf-8'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
+if vim.loader then
+  vim.loader.enable()
+end
 
-vim.wo.number = true
+_G.dd = function(...)
+  require("util.debug").dump(...)
+end
+vim.print = _G.dd
+
+require("config.lazy")
