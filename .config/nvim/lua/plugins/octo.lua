8481161d607215594849781bb4eb063149f02f73
void2610 <contact@void2610.dev>
2026-06-13 22:34:07 +0900

    feat: octo の <leader>gr を無効化して ghq ランチャーに譲る

--- .config/nvim/lua/plugins/octo.lua
+++ .config/nvim/lua/plugins/octo.lua
@@ -1,13 +1,10 @@
 return {
+  -- octo extra の <leader>gr (Octo repo list) は作成順固定で実用性が無いため無効化し、
+  -- 本来の <leader>gr を snacks.lua の ghq ランチャーに譲る。
   {
     "pwntester/octo.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "folke/snacks.nvim",
-    },
-    cmd = "Octo",
-    opts = {
-      picker = "snacks",
+    keys = {
+      { "<leader>gr", false },
     },
   },
 }
