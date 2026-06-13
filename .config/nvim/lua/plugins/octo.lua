fd46b18a5f66b9a73a23cb985adf057d815a0378
void2610 <contact@void2610.dev>
2026-06-13 19:58:42 +0900

    feat: Neovim に octo.nvim を追加する

---
+++ .config/nvim/lua/plugins/octo.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "pwntester/octo.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "folke/snacks.nvim",
+    },
+    cmd = "Octo",
+    opts = {
+      picker = "snacks",
+    },
+  },
+}
