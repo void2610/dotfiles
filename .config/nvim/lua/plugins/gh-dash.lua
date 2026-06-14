4cfe1e22b6214e621d01adb774efe503b1acdf60
void2610 <contact@void2610.dev>
2026-06-14 20:49:23 +0900

    feat: gh-dash 設定改善と Neovim 統合で現在リポジトリをコンテキスト保持

--- .config/nvim/lua/plugins/gh-dash.lua
+++ .config/nvim/lua/plugins/gh-dash.lua
@@ -5,8 +5,12 @@ return {
       {
         "<leader>gh",
         function()
+          -- 現在バッファのリポジトリで起動し、gh-dash の smartFilteringAtLaunch で当該リポジトリに自動スコープさせる。
+          local file = vim.api.nvim_buf_get_name(0)
+          local cwd = file ~= "" and vim.fs.dirname(file) or nil
           -- nix で宣言的に入れた gh-dash バイナリを snacks のフロートターミナルで開く。
           Snacks.terminal.toggle("gh-dash", {
+            cwd = cwd,
             win = {
               position = "float",
               width = 0.95,
