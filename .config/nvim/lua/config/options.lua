54c55516e070c4bbcb78e329df1b1fd9f8677525
Shuya Izumi <shuya.izumi@akariinc.co.jp>
2026-08-10 17:01:46 +0900

    feat: タブタイトルにブランチ・ファイル情報を表示する

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,3 +4,13 @@
 
 -- スペルチェックは typos-lsp (LSP) に一本化する。内蔵 spell は無効化する。
 vim.opt.spell = false
+
+-- タブタイトル用: gitsigns が検出したブランチ名を返す (git 外は空)
+function _G.TitleBranch()
+  local head = vim.b.gitsigns_head or vim.g.gitsigns_head
+  return head and ("(" .. head .. ")") or ""
+end
+
+-- ghostty 等のタブに「dir(branch)・file ●」を表示する
+vim.opt.title = true
+vim.opt.titlestring = "%{fnamemodify(getcwd(), ':t')}%{v:lua.TitleBranch()}・%t%{&modified ? ' ●' : ''}"
